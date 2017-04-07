11b8728f10805cd43e5eddfb09c1a6bc3882a69e
Bengt Weiße <bengtler@gmail.com>
2017-04-07 14:41:01 +0200

    Updates

--- app.69a42ff9b626513d0a99.js
+++ app.69a42ff9b626513d0a99.js
@@ -41961,6 +41961,12 @@ let AppComponent = class AppComponent {
             .subscribe(data => {
             console.log('native fromControl value changes with debounce', data);
         });
+        this.editor
+            .onContentChanged.debounceTime(400)
+            .distinctUntilChanged()
+            .subscribe(data => {
+            console.log('view child + directly subscription', data);
+        });
     }
     patchValue() {
         this.form.controls['editor'].patchValue(`${this.form.controls['editor'].value} patched!`);
