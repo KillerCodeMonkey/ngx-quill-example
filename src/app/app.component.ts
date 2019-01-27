8b12fa64d57960c9e831a3d9bacc3057f5738ca5
Bengt Weiße <bengtler@gmail.com>
2019-01-27 15:33:22 +0100

    feat(): use divs instead of p-tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core'
 
+import Quill from 'quill'
+
+const parchment = Quill.import('parchment')
+const block = parchment.query('block')
+block.tagName = 'DIV'
+// or class NewBlock extends Block {} NewBlock.tagName = 'DIV'
+Quill.register(block /* or NewBlock */, true)
+
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
