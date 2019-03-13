7a72a62f848c99d60be67bc812d4a649860288e8
Bengt Weiße <bengtler@gmail.com>
2019-03-13 20:16:03 +0100

    fix

---
+++ src/app/format-html/format-html.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-format-html',
+  templateUrl: './format-html.component.html'
+})
+export class FormatHtmlComponent {
+  htmlFormat = '<ul><li>1</li><li>2</li><li>3</li></ul>'
+}
