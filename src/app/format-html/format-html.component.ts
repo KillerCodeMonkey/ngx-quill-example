e4ae76289b004dce4be25517a4d61ce70cc9e115
Bengt Weiße <bengtler@gmail.com>
2019-03-20 20:42:10 +0100

    feat(htmlformat): mix ol und ul

--- src/app/format-html/format-html.component.ts
+++ src/app/format-html/format-html.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core'
   templateUrl: './format-html.component.html'
 })
 export class FormatHtmlComponent {
-  htmlFormat = '<ul><li>1</li><li>2</li><li>3</li></ul>'
+  htmlFormat = '<div>test</div><ul><li>1</li><li class="ql-indent-1">1-1</li><li>2</li><ol><li>numbered</li><li class="ql-indent-1">numbered-1</li></ol></ul><div><br></div>'
 }
