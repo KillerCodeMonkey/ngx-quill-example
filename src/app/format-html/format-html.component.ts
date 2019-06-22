a383f6aac710316f7961f108cbd899e3fd6c7b5d
Bengt Weiße <bengtler@gmail.com>
2019-06-22 12:33:59 +0200

    feat: html presentation

--- src/app/format-html/format-html.component.ts
+++ src/app/format-html/format-html.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-format-html',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core'
 })
 export class FormatHtmlComponent {
   htmlFormat = '<div>test</div><ul><li>1</li><li class="ql-indent-1">1-1</li><li>2</li><ol><li>numbered</li><li class="ql-indent-1">numbered-1</li></ol></ul><div><br></div>'
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  byPassHTML(html: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(html)
+  }
 }
