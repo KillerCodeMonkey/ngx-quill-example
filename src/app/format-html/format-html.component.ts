52b07cbd2b754f3ffe3eec6f24baf7a9f9a30ef2
Bengt Weiße <bengtler@gmail.com>
2019-06-25 18:57:38 +0200

    6.1.0

--- src/app/format-html/format-html.component.ts
+++ src/app/format-html/format-html.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
@@ -6,9 +7,17 @@ import { DomSanitizer } from '@angular/platform-browser'
   templateUrl: './format-html.component.html'
 })
 export class FormatHtmlComponent {
-  htmlFormat = '<div>test</div><ul><li>1</li><li class="ql-indent-1">1-1</li><li>2</li><ol><li>numbered</li><li class="ql-indent-1">numbered-1</li></ol></ul><div><br></div>'
+  form: FormGroup = this.fb.group({
+    html: new FormControl('<div>test</div><ul><li>1</li><li class="ql-indent-1">1-1</li><li>2</li><ol><li>numbered</li><li class="ql-indent-1">numbered-1</li></ol></ul><div><br></div>')
+  })
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.form.get('html').patchValue('<ol><li><sup>test</sup></li></ol>')
+    }, 4000)
+  }
 
   byPassHTML(html: string) {
     return this.sanitizer.bypassSecurityTrustHtml(html)
