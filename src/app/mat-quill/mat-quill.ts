e384c016c02eb8e98b73ea9fbb92f299fc858542
Bengt Weiße <bengtler@gmail.com>
2022-05-09 17:19:47 +0200

    update deps

--- src/app/mat-quill/mat-quill.ts
+++ src/app/mat-quill/mat-quill.ts
@@ -15,7 +15,15 @@ const SELECTOR = 'mat-quill'
   selector: SELECTOR,
   exportAs: 'matQuill',
   template: `
+    <ng-container *ngIf="toolbarPosition !== 'top'">
+      <div quill-editor-element *ngIf="!preserve"></div>
+      <pre quill-editor-element *ngIf="preserve"></pre>
+    </ng-container>
     <ng-content select="[quill-editor-toolbar]"></ng-content>
+    <ng-container *ngIf="toolbarPosition === 'top'">
+      <div quill-editor-element *ngIf="!preserve"></div>
+      <pre quill-editor-element *ngIf="preserve"></pre>
+    </ng-container>
     <ng-content></ng-content>
   `,
   inputs: ['disabled'],
