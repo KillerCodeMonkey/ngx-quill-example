3f4239823ad0e9c4ef6a17ace1ce36181465d92c
Bengt Weiße <bengtler@gmail.com>
2024-01-22 08:54:09 +0100

    fixifix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core'
 
-// import Quill from 'quill'
-// import Block from 'quill/blots/block'
+import Quill from 'quill'
+import Block from 'quill/blots/block'
 
-  // class NewBlock extends Block {
-  //    tagName = 'DIV'
-  // }
-  // Quill.register(NewBlock, true)
+class NewBlock extends Block {
+    
+}
+NewBlock.tagName = 'DIV'
+Quill.register(NewBlock, true)
 
 @Component({
   selector: 'app-root',
